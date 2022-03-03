7890e9db85a6740cadfebc74d9cf273f4a26591d
Audrey Yuan <audrey.yuan@mulesoft.com>
2022-03-02 17:13:44 -0800

    Change code to replace single quotes to double quotes so redshift json extract function can work

--- data_processor.py
+++ data_processor.py
@@ -4,12 +4,12 @@ def convert_struct_to_str(datum):
     fields = list(datum.keys())
     fields.remove("Tags")
     for field in fields:
-        datum[field] = str(datum[field])
+        datum[field] = str(datum[field]).replace("'", '"')
 
 f = open("instances.json")
 data = json.load(f)
 for datum in data["resources"]:
     convert_struct_to_str(datum)
 
-with open("new_resources.json", "w") as f:
+with open("new_resources.json", "w", encoding="utf-8") as f:
     json.dump(data, f, ensure_ascii=False, indent=4)
