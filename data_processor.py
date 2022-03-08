ea4a53495ac7c5d4005f4f4c484a866d56911ff3
Sean Zou <sean.zou@mulesoft.com>
2022-03-08 01:02:01 -0800

    adjust dest path for partitioning

--- data_processor.py
+++ data_processor.py
@@ -5,11 +5,14 @@ def convert_struct_to_str(datum):
     fields.remove("Tags")
     for field in fields:
         datum[field] = str(datum[field]).replace("'", '"')
-
-f = open("instances.json")
+file_name = "s3://cloud-governance-outputs/427000243349/us-east-1/ec2-tagging/2022/02/05/04/resources.json.gz"
+arr_fn = file_name.split("/", )
+dest_bucket_name = "processing-bucket"
+dest_file_path = arr_fn[0]+"/"+arr_fn[1]+"/"+dest_bucket_name+"/"+arr_fn[5]+"/"+arr_fn[6]+"/"+arr_fn[7]+"/"+arr_fn[8]+"/"+arr_fn[9]+"/"+arr_fn[3]+"/"+arr_fn[4]+"/"+arr_fn[10]
+f = open(file_name)
 data = json.load(f)
 for datum in data["resources"]:
     convert_struct_to_str(datum)
 
-with open("new_resources.json", "w", encoding="utf-8") as f:
+with open(dest_file_path, "w", encoding="utf-8") as f:
     json.dump(data, f, ensure_ascii=False, indent=4)
